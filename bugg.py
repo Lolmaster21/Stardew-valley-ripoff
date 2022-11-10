c8ce408c08c523867d9500a8c15a650efead0bf5
Lolmaster21 <78552751+Lolmaster21@users.noreply.github.com>
2022-11-10 09:46:11 -0700

    Create bugg.py

---
+++ bugg.py
@@ -0,0 +1,106 @@
+import pygame
+from settings import *
+from support import*
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self,pos,group):
+        super().__init__(group)
+        
+        self.import_assets()
+        self.status = 'down_idle'
+        self.frame_index = 0
+        
+        #General setup
+        self.image = self.animations[self.status][self.frame_index]
+        self.rect = self.image.get_rect(center = pos)
+        
+        #movement attributes
+        self.direction = pygame.math.Vector2()
+        self.pos = pygame.math.Vector2(self.rect.center)
+        self.speed = 200
+    
+        
+    def import_assets(self):
+        self.animations = {'up':[],'down':[],'left':[],'right':[],
+                           'right_idle':[],'left_idle':[],'up_idle':[],'down_idle':[],
+                           'right_hoe':[],'left_hoe':[],'up_hoe':[],'down_hoe':[],
+                           'right_axe':[],'left_axe':[],'up_axe':[],'down_axe':[],
+                           'right_water':[],'left_water':[],'up_water':[],'down_water':[]}
+    def animate(self,dt):
+        self.frame_index += 4*dt
+        if self.frame_index >=(self.animations[self.status]):
+            self.frame_index = 0 
+        self.image = self.animations[self.status][int(self.frame_index)]
+        
+        for animation in self.animations.keys():
+            full_path = '../graphics/character/' + animation
+            self.animations[animation]=import_folder(full_path)
+        print(self.animations)
+    
+    
+    
+    def input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP] and keys[pygame.K_RIGHT]:
+            self.direction.y = -1
+            self.direction.x = 1
+            self.status = "up"
+
+        if keys[pygame.K_UP] and keys[pygame.K_LEFT]:
+            self.direction.y = -1
+            self.direction.x = -1
+            self.status = "up"
+
+        if keys[pygame.K_DOWN] and keys[pygame.K_RIGHT]:
+            self.direction.y = 1
+            self.direction.x = 1
+            self.status = "down"
+
+        if keys[pygame.K_DOWN] and keys[pygame.K_LEFT]:
+            self.direction.y = 1
+            self.direction.x = -1
+            self.status = "down"
+
+        elif keys[pygame.K_UP]:
+            self.direction.y = -1
+            self.status = "up"
+
+        elif keys[pygame.K_DOWN]:
+            self.direction.y = 1
+            self.status = "down"
+        
+        elif keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+            self.status = "right"
+
+        
+        elif keys[pygame.K_LEFT]:
+            self.direction.x = -1
+            self.status = "left"
+
+        else:
+            self.direction.x = 0
+            self.direction.y = 0
+       
+        #print(self.direction)
+    
+    def move(self, dt):
+        if self.direction.magnitude() > 0:
+            self.direction = self.direction.normalize()
+        #print(self.direction)
+        self.pos.x += self.direction.x * self.speed * dt
+        self.rect.centerx = self.pos.x
+        #vertical movement
+        self.pos.y += self.direction.y * self.speed * dt
+        self.rect.centery = self.pos.y
+
+    def get_status(self):
+        if self.direction.magnitude() ==0:
+            self.status = self.status.split("_")[0]+ "idle"
+
+
+    def update(self,dt):
+        self.input()
+        self.move(dt)
+        self.get_status()
+        self.animate(dt)
