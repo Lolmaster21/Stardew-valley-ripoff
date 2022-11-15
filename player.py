95147b936e2646cc9816bcdba14450c5853f008a
Lolmaster21 <78552751+Lolmaster21@users.noreply.github.com>
2022-11-15 08:15:47 -0700

    Update player.py

--- player.py
+++ player.py
@@ -54,22 +54,29 @@ class Player(pygame.sprite.Sprite):
 
         elif keys[pygame.K_UP]:
             self.direction.y = -1
+            self.status = "up"
         
         elif keys[pygame.K_DOWN]:
             self.direction.y = 1
+            self.status = "down"
         
         elif keys[pygame.K_RIGHT]:
             self.direction.x = 1
-        
+            self.status = "right"
+
         elif keys[pygame.K_LEFT]:
             self.direction.x = -1
+            self.status = "left"
 
         else:
             self.direction.x = 0
             self.direction.y = 0
        
         #print(self.direction)
-    
+    def get_status(self):
+        if self.direction.magnitude() == 0:
+            self.status = self.status.split("_")[0]+ "_idle"
+
     def move(self, dt):
         if self.direction.magnitude() > 0:
             self.direction = self.direction.normalize()
@@ -79,7 +86,15 @@ class Player(pygame.sprite.Sprite):
         #vertical movement
         self.pos.y += self.direction.y * self.speed * dt
         self.rect.centery = self.pos.y
+    def animate(self,dt):
+       self.frame_index += 4*dt
+       if self.frame_index >= len(self.animations[self.status]):
+           self.frame_index = 0 
+       self.image = self.animations[self.status][int(self.frame_index)]
+
     
     def update(self,dt):
         self.input()
+        self.get_status()
         self.move(dt)
+        self.animate(dt)
