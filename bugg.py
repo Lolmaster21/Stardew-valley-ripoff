25829da9c9e89acab8c4579ee2ce6fdcf8c5c553
Lolmaster21 <78552751+Lolmaster21@users.noreply.github.com>
2022-11-17 09:22:06 -0700

    Update bugg.py

--- bugg.py
+++ bugg.py
@@ -1,4 +1,5 @@
 import pygame
+from timer import *
 from settings import *
 from support import*
 
@@ -8,6 +9,7 @@ class Player(pygame.sprite.Sprite):
         
         self.import_assets()
         self.status = 'down_idle'
+        self.selected_tool = "water"
         self.frame_index = 0
         
         #General setup
@@ -18,6 +20,16 @@ class Player(pygame.sprite.Sprite):
         self.direction = pygame.math.Vector2()
         self.pos = pygame.math.Vector2(self.rect.center)
         self.speed = 200
+        
+        self.timers = {
+            "tool use": Timer(350, self.use_tool)    
+        }
+
+        self.selected_tool = "axe"
+
+    def use_tool(self):
+        print(self.selected_tool)
+
     
         
     def import_assets(self):
@@ -36,46 +48,60 @@ class Player(pygame.sprite.Sprite):
     
     def input(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_UP] and keys[pygame.K_RIGHT]:
-            self.direction.y = -1
-            self.direction.x = 1
+        if not self.timers['tool use']. active:
+            if keys[pygame.K_UP] and keys[pygame.K_RIGHT]:
+                self.direction.y = -1
+                self.direction.x = 1
 
-        if keys[pygame.K_UP] and keys[pygame.K_LEFT]:
-            self.direction.y = -1
-            self.direction.x = -1
+            if keys[pygame.K_UP] and keys[pygame.K_LEFT]:
+                self.direction.y = -1
+                self.direction.x = -1
 
-        if keys[pygame.K_DOWN] and keys[pygame.K_RIGHT]:
-            self.direction.y = 1
-            self.direction.x = 1
+            if keys[pygame.K_DOWN] and keys[pygame.K_RIGHT]:
+                self.direction.y = 1
+                self.direction.x = 1
 
-        if keys[pygame.K_DOWN] and keys[pygame.K_LEFT]:
-            self.direction.y = 1
-            self.direction.x = -1
+            if keys[pygame.K_DOWN] and keys[pygame.K_LEFT]:
+                self.direction.y = 1
+                self.direction.x = -1
 
-        elif keys[pygame.K_UP]:
-            self.direction.y = -1
-            self.status = "up"
+            if keys[pygame.K_SPACE]:
+                self.timers['tool use'].activate()
+                self.direction = pygame.math.Vector2()
+                self.frame_index = 0
+
+            elif keys[pygame.K_UP]:
+                self.direction.y = -1
+                self.status = "up"
         
-        elif keys[pygame.K_DOWN]:
-            self.direction.y = 1
-            self.status = "down"
+            elif keys[pygame.K_DOWN]:
+                self.direction.y = 1
+                self.status = "down"
         
-        elif keys[pygame.K_RIGHT]:
-            self.direction.x = 1
-            self.status = "right"
+            elif keys[pygame.K_RIGHT]:
+                self.direction.x = 1
+                self.status = "right"
 
-        elif keys[pygame.K_LEFT]:
-            self.direction.x = -1
-            self.status = "left"
+            elif keys[pygame.K_LEFT]:
+                self.direction.x = -1
+                self.status = "left"
 
-        else:
-            self.direction.x = 0
-            self.direction.y = 0
+            else:
+                self.direction.x = 0
+                self.direction.y = 0
        
         #print(self.direction)
     def get_status(self):
-        if self.direction.magnitude() ==0:
-            self.status += self.status.split("_")[0]+ "_idle"
+        if self.direction.magnitude() == 0:
+            self.status = self.status.split("_")[0] + "_idle"
+
+            if self.timers['tool use']. activate:
+                print("tool is being used")
+                self.status = self.status.split("_")[0] + "_" + self.selected_tool
+
+    def update_timers():
+        for timer in self.timers.values():
+            timer.update
 
     def move(self, dt):
         if self.direction.magnitude() > 0:
@@ -90,11 +116,12 @@ class Player(pygame.sprite.Sprite):
        self.frame_index += 4*dt
        if self.frame_index >= len(self.animations[self.status]):
            self.frame_index = 0 
-           self.image = self.animations[self.status][int(self.frame_index)]
+       self.image = self.animations[self.status][int(self.frame_index)]
 
     
     def update(self,dt):
         self.input()
         self.get_status()
+        self.update_timers()
         self.move(dt)
         self.animate(dt)
